773864355315672dd91537e8909a4d4565711ac9
Noxtar <alvaro210111@gmail.com>
2026-09-13 16:28:30 +0100

    Create calculatorprogram.py

---
+++ calculatorprogram.py
@@ -0,0 +1,28 @@
+print("********************")
+print("*Calculadora Simple*")
+num1=int(input("********************\n"))
+
+while True:
+    op=input()
+    num2=int(input())
+
+    if op=="+":
+        num1+=num2
+        print(round(num1, 2))
+        continue
+    elif op=="-":
+        num1-=num2
+        print(round(num1, 2))
+        continue
+    elif op=="*":
+        num1*=num2
+        print(round(num1, 2))
+        continue
+    elif op=="/":
+        num1/=num2
+        print(round(num1, 2))
+        continue
+    else:
+        print("esa opción no está contemplada")
+        break
+    
