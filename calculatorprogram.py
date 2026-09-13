f0a34b6c443dff48b44d3c819955fe5d4caaeead
Noxtar <alvaro210111@gmail.com>
2026-09-13 16:49:35 +0100

    Update calculatorprogram.py

--- calculatorprogram.py
+++ calculatorprogram.py
@@ -2,27 +2,43 @@ print("********************")
 print("*Calculadora Simple*")
 num1=int(input("********************\n"))
 
-while True:
-    op=input()
-    num2=int(input())
+if num1==int:
+    while True:
+        op=input()
+        num2=int(input())
+        if op=="+":
+                num1+=num2
+                print(round(num1, 2))
+                continue
+        elif op=="-":
+                num1-=num2
+                print(round(num1, 2))
+                continue
+        elif op=="*":
+                num1*=num2
+                print(round(num1, 2))
+                continue
+        elif op=="/":
+                num1/=num2
+                print(round(num1, 2))
+                continue
+        elif op=="//":
+                    num1//=num2
+                    print(round(num1, 2))
+                    continue
+        elif op=="%":
+                    num1%=num2
+                    print(round(num1, 2))
+                    continue
+        elif op=="**":
+                    num1**=num2
+                    print(round(num1, 2))
+                    continue
+        else:
+                print("esa opción no está contemplada")
+                break
+else:
+        print("Recuerde introducir un valor entero")    
 
-    if op=="+":
-        num1+=num2
-        print(round(num1, 2))
-        continue
-    elif op=="-":
-        num1-=num2
-        print(round(num1, 2))
-        continue
-    elif op=="*":
-        num1*=num2
-        print(round(num1, 2))
-        continue
-    elif op=="/":
-        num1/=num2
-        print(round(num1, 2))
-        continue
-    else:
-        print("esa opción no está contemplada")
-        break
+
     
